5ab7152d8b2142cfc8b793a74e101b0922ff567b
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-05 09:14:32 -0700

    nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,58 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+var USER_DATA = {
+  name: 'Michael Cruz',
+  username: 'MichaelRCruz',
+  image: 'https://avatars0.githubusercontent.com/u/17737318?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div>Hello, wolkknknknkrld!</div>
-    )
+/*
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
   }
 });
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('app')
-);
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
